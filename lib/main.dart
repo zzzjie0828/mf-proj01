8fb3501460505ac00c2fbf376d9f5fd3a78b9d75
zzzjie0828 <zzzjie1993@163.com>
2024-04-12 18:25:56 +0800

    route、pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:zproj01/app/conf/themes/app_theme.dart';
 
+import 'app/conf/routes/app_pages.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,6 +18,8 @@ class MyApp extends StatelessWidget {
       title: 'My Proj01',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.basic,
+      initialRoute: AppPages.initial,
+      getPages: AppPages.routes,
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
